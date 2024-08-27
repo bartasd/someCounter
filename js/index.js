b6d1bde14a0961dd09f27aad74ff5a64d67b6f7d
Bartas Dausynas <bartas3@gmail.com>
2024-08-27 13:40:59 +0300

    bugs fixed

--- js/index.js
+++ js/index.js
@@ -1,63 +1,70 @@
+const container = document.getElementById("cont");
 let num = prompt("Please input a count: ");
 while (true) {
-  const valid = !isNaN(num) && num % 1 === 0;
-  if (valid) break;
-  else {
+  const valid = !isNaN(num) && +num % 1 === 0 && +num >= 0;
+  if (valid) {
+    num = +num;
+    if (num === 0) showMessage();
+    else doStuff();
+    break;
+  } else {
     num = prompt(
-      "A count should be a WHOLE NUMBER, YOU MORRON!!! Please try again..."
+      "A count should be a WHOLE AND POSITIVE NUMBER,!!! Please try again..."
     );
   }
 }
-const numArr = [...(num + "")];
-const len = numArr.length;
-const container = document.getElementById("cont");
-container.style.width = `${73.67 * len + 15}px`;
 
-for (let p = 0; p < len; p++) {
-  const pTag = document.createElement("p");
-  pTag.classList.add("pnum");
-  pTag.innerText = numArr[p];
-  container.insertAdjacentElement("beforeend", pTag);
-}
+function doStuff() {
+  const numArr = [...(num + "")];
+  const len = numArr.length;
+  container.style.width = `${73.67 * len + 15}px`;
 
-const digits = container.getElementsByClassName("pnum");
-
-let i = num;
-
-const myInterval = setInterval(changeCounter, 50);
-
-function changeCounter() {
-  i--;
-  const currN = i + "";
-  const currWide = currN.length;
-  container.style.width = `${73.67 * currWide + 15}px`;
-  const current = [...currN.padStart(len, "0")];
-  for (let k = len - 1; k >= 0; k--) {
-    const curr = current[k];
-    if (curr !== digits[k].innerText) {
-      if (k != len - 1) {
-        digits[k].classList.add("flip-animation");
-        setTimeout(() => {
-          digits[k].innerText = curr;
-        }, 125);
-        digits[k].addEventListener(
-          "animationend",
-          () => digits[k].classList.remove("flip-animation"),
-          { once: true }
-        );
-      } else {
-        digits[k].innerText = curr;
-      }
-    }
-    if (curr == "0" && current.slice(0, k + 1).every((e) => e == "0")) {
-      if (!digits[k].classList.contains("hidden")) {
-        digits[k].classList.add("hidden");
-      }
-    }
+  for (let p = 0; p < len; p++) {
+    const pTag = document.createElement("p");
+    pTag.classList.add("pnum");
+    pTag.innerText = numArr[p];
+    container.insertAdjacentElement("beforeend", pTag);
   }
-  if (i == 0) {
-    clearInterval(myInterval);
-    showMessage();
+
+  const digits = container.getElementsByClassName("pnum");
+
+  let i = num;
+
+  const myInterval = setInterval(changeCounter, 50);
+
+  function changeCounter() {
+    i--;
+    const currN = i + "";
+    const currWide = currN.length;
+    container.style.width = `${73.67 * currWide + 15}px`;
+    const current = [...currN.padStart(len, "0")];
+    for (let k = len - 1; k >= 0; k--) {
+      const curr = current[k];
+      if (curr !== digits[k].innerText) {
+        if (k != len - 1) {
+          digits[k].classList.add("flip-animation");
+          setTimeout(() => {
+            digits[k].innerText = curr;
+          }, 125);
+          digits[k].addEventListener(
+            "animationend",
+            () => digits[k].classList.remove("flip-animation"),
+            { once: true }
+          );
+        } else {
+          digits[k].innerText = curr;
+        }
+      }
+      if (curr == "0" && current.slice(0, k + 1).every((e) => e == "0")) {
+        if (!digits[k].classList.contains("hidden")) {
+          digits[k].classList.add("hidden");
+        }
+      }
+    }
+    if (i == 0) {
+      clearInterval(myInterval);
+      showMessage();
+    }
   }
 }
 
